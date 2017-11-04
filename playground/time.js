08be9fc991cf62af3e5c6b0cde05324023003587
Tuan Anh Ha <hatuananhvnhn@gmail.com>
2017-11-04 18:04:07 +0100

    timestamp

---
+++ playground/time.js
@@ -0,0 +1,10 @@
+// var date = new Date();
+//
+// console.log(date.getMonth()); // 10 = November
+
+var moment = require('moment');
+
+var date   = moment(); // current time
+
+// console.log(date.format('Do MMM, YYYY')); // Nov
+// console.log(date.format('HH:mma'));
